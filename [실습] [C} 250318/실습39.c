fb32416508ec50c57b64a4359b0aae8b4dc86db5
Kim-Byeong-Ju <bangzoo@daou.co.kr>
2025-03-18 16:01:59 +0900

    0318 실습

---
+++ [실습] [C} 250318/실습39.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	int* p1 = malloc(sizeof(int));
+	int* p2 = calloc(1, sizeof(int));
+
+	printf("%d, %d", *p1, *p2);
+
+	free(p1);
+	free(p2);
+	return 0;
+}
